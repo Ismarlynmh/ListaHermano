b0a4fcf2afec74e57a0cdbb00f2b1a351d9e2be3
Ismarlynmh <ismarlyn123@gmail.com>
2022-08-03 19:41:03 -0300

    agregando en base de datos

--- app/src/main/java/com/mendozacreations/listahermano/di/AppModule.kt
+++ app/src/main/java/com/mendozacreations/listahermano/di/AppModule.kt
@@ -1,12 +1,18 @@
 package com.mendozacreations.listahermano.di
 
+import android.content.Context
+import androidx.room.Room
+import com.mendozacreations.listahermano.data.dto.AsistenciaDao
+import com.mendozacreations.listahermano.data.dto.AsistenciasDb
 import com.mendozacreations.listahermano.data.remote.HermanoAPI
+import com.mendozacreations.listahermano.data.repository.AsistenciaRepository
 import com.mendozacreations.listahermano.data.repository.HermanoRepository
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -38,4 +44,27 @@ object AppModule {
     fun provideHermanoRepository(hermanoAPI: HermanoAPI): HermanoRepository {
         return HermanoRepository(hermanoAPI)
     }
+/*
+    @Provides
+    @Singleton
+    fun providesAsistenciasDb(@ApplicationContext context: Context):AsistenciasDb{
+        return  Room.databaseBuilder(
+            context,
+            AsistenciasDb::class.java,
+            "AsistenciasDb")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+
+    @Provides
+    fun providesAsistenciaDao(asistenciasDb: AsistenciasDb): AsistenciaDao {
+        return  AsistenciasDb.asistenciaDao
+    }
+
+
+    @Provides
+    fun providesAsistenciaRepository(asistenciaDao: AsistenciasDb): AsistenciaRepository {
+        return  AsistenciaRepository(asistenciaDao)
+    }*/
 }
