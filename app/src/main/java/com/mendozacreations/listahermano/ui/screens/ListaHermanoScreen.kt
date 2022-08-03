ff1898420733cc534c6d2b44edb21e34c88c3e81
Ismarlynmh <ismarlyn123@gmail.com>
2022-08-03 19:46:30 -0300

    agregando boton de lista de asistencia

--- app/src/main/java/com/mendozacreations/listahermano/ui/screens/ListaHermanoScreen.kt
+++ app/src/main/java/com/mendozacreations/listahermano/ui/screens/ListaHermanoScreen.kt
@@ -1,18 +1,27 @@
 package com.mendozacreations.listahermano
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.EditNote
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.mendozacreations.listahermano.data.dto.HermanoDto
 import com.mendozacreations.listahermano.model.HermanoViewModel
+import com.mendozacreations.listahermano.ui.theme.ListaHermanoTheme
 
 @Composable
 fun ListaHermanoScreen(
@@ -25,7 +34,23 @@ fun ListaHermanoScreen(
             TopAppBar(title = { Text( "AppList",
                 fontFamily = FontFamily.Default,
                 fontWeight = FontWeight.Black
-            ) })
+            ) },
+
+                    actions = {
+                IconButton(onClick = { navHostController.navigate("AsistenciaListScreen")
+
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "EDITAR",
+                        modifier = Modifier.size(30.dp)
+                    )
+
+                }
+            },
+
+        )
+
         },
         floatingActionButton = {
             FloatingActionButton(
@@ -37,6 +62,8 @@ fun ListaHermanoScreen(
                 Icon(imageVector = Icons.Default.Add, contentDescription = "Hermano")
             }
         },
+
+
         scaffoldState = ScaffoldState
     ){it
 
@@ -53,4 +80,12 @@ fun ListaHermanoScreen(
                 CircularProgressIndicator()
         }
     }
-}
+
+}/*
+@Preview
+@Composable
+private fun  PreviewHermList(){
+    ListaHermanoTheme{
+        ListaHermanoScreen()
+    }
+}*/
