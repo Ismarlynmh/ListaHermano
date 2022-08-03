47bac7b5faf2c7586ab9e8c5001dd3b81c6656b1
Ismarlynmh <ismarlyn123@gmail.com>
2022-08-03 20:07:26 -0300

    Screens de Registro y Lista de Asistencia creado

--- app/src/main/java/com/mendozacreations/listahermano/util/Screen.kt
+++ app/src/main/java/com/mendozacreations/listahermano/util/Screen.kt
@@ -3,4 +3,6 @@ package com.mendozacreations.listahermano.util
 sealed class Screen(val route: String) {
     object RegistroHermanoScreen: Screen("RegistroHermanoScreen")
     object ListaHermanoScreen: Screen("ListaHermanoScreen")
+    object RegistroAsistenciaScreen: Screen("RegistroAsistenciaScreen")
+    object AsistenciaListScreen: Screen("AsistenciaListScreen")
 }
